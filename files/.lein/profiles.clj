40535e66a03f4554dfd4b779bf3faaad6a6f9a51
Basel Farah <baself@gmail.com>
2015-09-25 09:29:03 +0100

    added cider/cider-nrepl to .lein/profiles

--- files/.lein/profiles.clj
+++ files/.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-snapshot"]
-                                    [refactor-nrepl "1.1.0"]]
+                  [refactor-nrepl "1.1.0"]
+                  [cider/cider-nrepl "0.9.1"]]
                 :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
