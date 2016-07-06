b89ff0ffd6f7ac89842a4975065877a83b014e5b
Basel Farah <baself@gmail.com>
2016-07-06 09:57:27 +0100

    fix versions

--- files/.lein/profiles.clj
+++ files/.lein/profiles.clj
@@ -1,3 +1,6 @@
-{:user {:plugins [[refactor-nrepl "1.2.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.10.0-snapshot"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+{:user {:plugins [[refactor-nrepl "2.2.0"]
+                  [cider/cider-nrepl "0.12.0"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [pjstadig/humane-test-output "0.7.0"]]}}
